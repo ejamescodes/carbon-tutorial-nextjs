6be9bdf4e8f77a0ff91c3f7ba1f3eef3c3a28c0d
Ernest Perez <Ernest.Perez@ibm.com>
2024-11-21 15:22:35 -0500

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
+'use client';
 
-export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
+import { Button } from '@carbon/react';
+import LandingPage from './home/page';
 
-  )
+// export default function Home() {
+//   return (
+// //     <div>
+// // Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+// //     </div>
+//     <Button>Button</Button>
+//   )
+// }
+
+export default function Page() {
+  return <LandingPage />;
 }
